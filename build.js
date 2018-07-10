a88b57d42de4337aea3a3ed4cea2dfef7c44f0fd
dustinpfister <dustin.pfister@gmail.com>
2018-07-10 14:32:11 -0400

    getting internals

--- build.js
+++ build.js
@@ -2,19 +2,40 @@ let klaw = require('klaw'),
 fs = require('fs-extra'),
 through2 = require('through2'),
 marked = require('marked'),
+cheerio = require('cheerio'),
 
 // dir to the posts
 dir_posts = '../blog_posts/_posts';
 
+// klaw over folder
 klaw(dir_posts)
 
+// for each file
 .pipe(through2.obj(function (item, enc, next) {
 
         let self = this;
 
+        item.internals = []
+
+        // read the file
         fs.readFile(item.path).then(function (data) {
 
+            // parse to html with marked
             let html = marked(data.toString());
+
+            let $ = cheerio.load(html);
+
+            $('a').each(function (el, b) {
+
+                let match = b.attribs.href.match(/^\//);
+
+                if (match) {
+
+                    item.internals.push(match.input);
+
+                }
+
+            })
 
             self.push(item);
             next();
@@ -29,6 +50,6 @@ klaw(dir_posts)
 
 .on('data', function (item) {
 
-    //console.log(item.path)
+    console.log(item)
 
 });
